73e9020a460c7dc8001af583af1752bc4a8ba942
samar <samar@avionte.com>
2013-04-03 15:46:12 -0500

    Third changes
    more changes

--- OneApp2/Controllers/HomeController.cs
+++ OneApp2/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace OneApp2.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "This is a new application for My Site HAi";
 
             return View();
         }
